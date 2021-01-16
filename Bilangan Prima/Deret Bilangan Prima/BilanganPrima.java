6e782e9bd7af9b53c2140dad9a810516bd644f51
Fajar-ab <fajarfanz378@gmail.com>
2021-01-17 01:59:16 +0700

    program bilangn prima

--- Bilangan Prima/Deret Bilangan Prima/BilanganPrima.java
+++ Bilangan Prima/Deret Bilangan Prima/BilanganPrima.java
@@ -1,6 +1,24 @@
-public class BilanganPrina {
+import java.util.Scanner;
+
+class BilanganPrima {
 
     public static void main(String[] args){
-      System.out.println("Hello, World");
+    	Scanner userIn = new Scanner(System.in);
+
+    	System.out.print("Input n : ");
+    	int n = userIn.nextInt();
+
+    	for (int i = 2; i <= n; i++) {
+    		int k = 0;
+    		for(int j = 1; j <= i; j++) {
+    			if(i % j == 0) {
+    				k++;
+    			}
+    		}
+
+    		if(k == 2) {
+    			System.out.print(i + " ");
+    		}
+    	}
     }
 }
